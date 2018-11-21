2c5ab32f3feff76f1782930c17d35f851e18bdba
raymond <lm.lu@mail.utoronto.ca>
2018-11-20 23:23:56 -0400

    Improved Hangman so it can generate random words from vocabulary list

--- Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/Word.java
+++ Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/Word.java
@@ -1,7 +1,10 @@
 package group0642.csc207.fall18.gamecenter;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
+import java.util.Random;
+import java.io.File;
 
 public class Word {
     private String word;
@@ -10,15 +13,33 @@ public class Word {
     private String display;
     private int health;
 
-    public Word(String word){
-        this.word = word;
+    Word(){
+        setWord();
         this.length = word.length();
         this.entered = new ArrayList<>();
         this.display = StringUtils.repeat("_", this.length);
         this.health = 10;
+
     }
 
-    public String validEnter(String letter){
+    void setWord(){
+        Random rand = new Random();
+        int n = rand.nextInt(390);
+        File file = new File("src/vocabulary_list.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            for(int i = 0; i < n; i++){
+                sc.nextLine();
+            }
+            this.word = sc.nextLine();
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String validEnter(String letter){
         if(letter.length() != 1){
             return "Length must be 1!";
         } //Check if letter is length one
@@ -34,27 +55,27 @@ public class Word {
         return "Pass"; //Check if letter is Capital Letter
     }
 
-    public boolean check(int position, String letter){
+    boolean check(int position, String letter){
         return String.valueOf(this.word.charAt(position)).equals(letter);
     }
 
-    public ArrayList<String> getEntered(){
+    ArrayList<String> getEntered(){
         return entered;
     }
 
-    public int getLength(){
+    int getLength(){
         return length;
     }
 
-    public int getHealth(){
+    int getHealth(){
         return health;
     }
 
-    public boolean win(){
+    boolean win(){
         return word.equals(display);
     }
 
-    public String getDisplay(){
+    String getDisplay(){
         String s = "";
         for(int i = 0; i < display.length(); i++){
             s = s + display.charAt(i) + " ";
@@ -62,7 +83,7 @@ public class Word {
         return s;
     }
 
-    public String enter(String entered){
+    String enter(String entered){
         entered = entered.toLowerCase();
         String good = validEnter(entered);
         if(good.equals("Pass")){
@@ -82,7 +103,7 @@ public class Word {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Word w = new Word("asdwz");
+        Word w = new Word();
         while(w.getHealth() != 0 && !w.win()) {
             String message = w.enter(sc.nextLine());
             if(message.equals("Pass")){
