7f13b7ea29ba121ac8a7aba0515c0bb1bb4a27d3
raymondlu <lm.lu@mail.utoronto.ca>
2018-11-26 16:36:37 -0500

    Deleted some imports that no longer used and wrote unit test for class Word.

--- Phase2/GameCenter/app/src/test/java/group0642/csc207/fall18/gamecenter/WordTest.java
+++ Phase2/GameCenter/app/src/test/java/group0642/csc207/fall18/gamecenter/WordTest.java
@@ -13,4 +13,37 @@ public class WordTest {
         assertEquals(w.getHealth(), 10);
         assertFalse(w.win());
     }
+
+    @Test
+    public void testSetHealthMethod() {
+        w = new Word("apple");
+        w.setHealth(15);
+        assertEquals(w.getHealth(), 15);
+    }
+
+    @Test
+    public void testEnterAndGetDisplayMethodsAndWin() {
+        w = new Word("apple");
+        assertEquals(w.getDisplay(), "_ _ _ _ _");
+        w.enter("a");
+        assertEquals(w.getDisplay(), "a _ _ _ _");
+        w.enter("p");
+        w.enter("l");
+        assertFalse(w.win());
+        w.enter("e");
+        assertEquals(w.getDisplay(), "a p p l e");
+        assertTrue(w.win());
+    }
+
+    @Test
+    public void testEnteredWrongLetterAndGetHealthMethod() {
+        w = new Word("apple");
+        w.setHealth(2);
+        w.enter("t");
+        assertEquals(w.getDisplay(), "_ _ _ _ _");
+        assertEquals(w.getHealth(), 1);
+        w.enter("v");
+        assertEquals(w.getHealth(), 0);
+    }
+
 }
