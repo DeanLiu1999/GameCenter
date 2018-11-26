96b1f9ee50cdce2f8583be42d8b4618db40a2136
raymondlu <lm.lu@mail.utoronto.ca>
2018-11-26 14:05:47 -0500

    We decided to use button a to z for input of hangman, so I deleted some unnecessarily check and message toast.

--- Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/Word.java
+++ Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/Word.java
@@ -7,83 +7,58 @@ import org.apache.commons.lang3.StringUtils;
 public class Word implements Serializable {
     private String word;
     private int length;
-    private ArrayList<String> entered;
     private String display;
     private int health;
 
-    Word(String word){
+    Word(String word) {
         this.word = word;
         this.length = word.length();
-        this.entered = new ArrayList<>();
         this.display = StringUtils.repeat("_", this.length);
         this.health = 10;
     }
 
-    private String validEnter(String letter){
-        if(letter.length() != 1){
-            return "Length must be 1!";
-        } //Check if letter is length one
-        char c = letter.charAt(0);
-        int i = (int)c;
-        if(i < 97 || i > 122){
-            return "Must be letter!";
-        }
-        if(this.entered.contains(letter)){
-            return "Already entered";
-        }
-        this.entered.add(letter);
-        return "Pass"; //Check if letter is Capital Letter
-    }
-
-    private boolean check(int position, String letter){
+    private boolean check(int position, String letter) {
         if (this.word.length() >= position)
             return String.valueOf(this.word.charAt(position)).equals(letter);
         return false;
     }
 
-    ArrayList<String> getEntered(){
-        return entered;
-    }
-
-    void setHealth(int health){
+    void setHealth(int health) {
         this.health = health;
     }
 
-    int getHealth(){
+    int getHealth() {
         return health;
     }
 
-    boolean win(){
+    boolean win() {
         return word.equals(display);
     }
 
-    String getDisplay(){
+    String getDisplay() {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < display.length(); i++){
+        for (int i = 0; i < display.length(); i++) {
             sb.append(display.charAt(i));
             sb.append(" ");
         }
         return sb.toString();
     }
 
-    String enter(String entered){
-        entered = entered.toLowerCase();
-        String good = validEnter(entered);
-        if(good.equals("Pass")){
-            boolean found = false;
-            for(int i = 0; i < this.length; i++){
-                if(this.check(i, entered)){
-                    display = display.substring(0, i) + entered + display.substring(i + 1);
-                    found = true;
-                }
-            }
-            if(!found){
-                health--;
+    void enter(String entered) {
+        boolean found = false;
+        for (int i = 0; i < this.length; i++) {
+            if (this.check(i, entered)) {
+                display = display.substring(0, i) + entered + display.substring(i + 1);
+                found = true;
             }
         }
-        return good;
+        if (!found) {
+            health--;
+        }
     }
+}
 
+/*
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Word w = new Word("Whatever");
@@ -97,4 +72,4 @@ public class Word implements Serializable {
             }
         }
     }
-}
+    */
