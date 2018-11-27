37ebe3ef9418bec2a54ec3b0db9223edc6991101
lucheng9 <ch.lu@mail.utoronto.ca>
2018-11-27 12:48:37 -0500

    Refactored to separate logic from display.

--- Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/StateManager.java
+++ Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/StateManager.java
@@ -36,7 +36,7 @@ class StateManager implements Serializable {
     }
 
     void stand() {
-            ++stageC;
+        ++stageC;
     }
 
     int getStageC() {
@@ -50,7 +50,10 @@ class StateManager implements Serializable {
     boolean gameEnd() {
         return stageC >= 1 || getPlayerScore() == 0;
     }
-    boolean gameWin(){ return getPlayerScore() >= getComputerScore(); }
+
+    boolean gameWin() {
+        return getPlayerScore() >= getComputerScore();
+    }
 
     String[] getComputerCardsStr() {
         String[] s = new String[6];
@@ -67,9 +70,45 @@ class StateManager implements Serializable {
         }
         return s;
     }
-    
-    boolean initialStage(){
+
+    boolean initialStage() {
         return getStageP() == 1 && getStageC() == 0;
+    }
+
+    private Integer[] strToCardsId(String[] cardStr) {
+        Integer[] cardsId = {R.drawable.club_a, R.drawable.club_2, R.drawable.club_3, R.drawable.club_4,
+                R.drawable.club_5, R.drawable.club_6, R.drawable.club_7, R.drawable.club_8,
+                R.drawable.club_9, R.drawable.club_10, R.drawable.club_j, R.drawable.club_q,
+                R.drawable.club_k,};
+        Integer[] cards = new Integer[6];
+        for (int i = 0; i < 6; i++) {
+            switch (cardStr[i]) {
+                case "A":
+                    cards[i] = cardsId[0];
+                    break;
+                case "J":
+                    cards[i] = cardsId[10];
+                    break;
+                case "Q":
+                    cards[i] = cardsId[11];
+                    break;
+                case "K":
+                    cards[i] = cardsId[12];
+                    break;
+                default:
+                    cards[i] = cardsId[Integer.parseInt(cardStr[i]) - 1];
+                    break;
+            }
+        }
+        return cards;
+    }
+
+    Integer[] getComputerCardsId() {
+        return strToCardsId(getComputerCardsStr());
+    }
+
+    Integer[] getPlayerCardsId() {
+        return strToCardsId(getPlayerCardsStr());
     }
 }
 
