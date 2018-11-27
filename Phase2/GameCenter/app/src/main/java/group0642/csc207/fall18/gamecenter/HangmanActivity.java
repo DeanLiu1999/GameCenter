542991528b6d635a5161302f8534e79e53b4e6fc
Yilun Li <bobliyilun.li@mail.utoronto.ca>
2018-11-27 10:47:33 -0500

    now the user is able to load their last game in the infinity mode of hangman

--- Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/HangmanActivity.java
+++ Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/HangmanActivity.java
@@ -36,10 +36,12 @@ public class HangmanActivity extends AppCompatActivity {
     private Random randomGenerator;
     private ArrayList<String> wordList;
     private Button showScoreboard_1;
+    private Button saveButton;
+    private Button[] entries;
+    private ArrayList entered = new ArrayList();
 
-
-    private String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    public static boolean load = false;
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     @Override
 
@@ -48,7 +50,7 @@ public class HangmanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
 
-        Button[] entries = new Button[]{findViewById(R.id.a), findViewById(R.id.b), findViewById(R.id.c),
+        entries = new Button[]{findViewById(R.id.a), findViewById(R.id.b), findViewById(R.id.c),
                 findViewById(R.id.d), findViewById(R.id.e), findViewById(R.id.f), findViewById(R.id.g),
                 findViewById(R.id.h), findViewById(R.id.i), findViewById(R.id.j), findViewById(R.id.k),
                 findViewById(R.id.l), findViewById(R.id.m), findViewById(R.id.n), findViewById(R.id.o),
@@ -64,14 +66,28 @@ public class HangmanActivity extends AppCompatActivity {
         healthBar = findViewById(R.id.healthBar);
         update();
         showScoreboard_1 = findViewById(R.id.showScoreboard_1);
+        saveButton = findViewById(R.id.saveButton_1);
 
         Intent intent = getIntent();
         name = intent.getStringExtra("name");
         game = intent.getStringExtra("game");
-
+        setSaveButton();
         setShowScoreboard_1Listener();
         buttonListActions(entries, alphabet);
+        if (load) {
+            loadGame();
+            load = false;
+        }
 
+    }
+
+    public void setSaveButton() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
     }
 
     public void setShowScoreboard_1Listener() {
@@ -79,7 +95,6 @@ public class HangmanActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (answer.getHealth() == 0) {
-                    showScoreboard_1.setText("Show scoreboard");
                     switchToScore(name, game, score);
                 } else {
                     switchToStart(name, game);
@@ -103,20 +118,20 @@ public class HangmanActivity extends AppCompatActivity {
         HangmanActivity.this.startActivity(startOver);
     }
 
-    public void buttonListActions(Button[] lst, String[] lstOfStrings) {
+    public void buttonListActions(Button[] lst, String strings) {
         int i = 0;
         while (i < lst.length) {
 
-            buttonListListener(lst[i], lst, lstOfStrings[i]);
+            buttonListListener(lst[i], lst, strings.charAt(i));
             i++;
         }
     }
 
-    public void buttonListListener(final Button b, final Button[] lst, final String string) {
+    public void buttonListListener(final Button b, final Button[] lst, final char letter) {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                letterEntryListener(lst, b, string);
+                letterEntryListener(lst, b, letter);
             }
         });
     }
@@ -128,7 +143,6 @@ public class HangmanActivity extends AppCompatActivity {
 
     }
 
-
     public String chooseTheAnswer() {
         int index = randomGenerator.nextInt(wordList.size());
         return wordList.get(index);
@@ -139,8 +153,10 @@ public class HangmanActivity extends AppCompatActivity {
         healthBar.setText(String.valueOf(answer.getHealth()));
     }
 
+    public void letterEntryListener(Button[] lst, Button button, char letter) {
 
-    public void letterEntryListener(Button[] lst, Button button, String str) {
+        entered.add(letter);
+        String str = String.valueOf(letter);
         answer.enter(str);
         button.setEnabled(false);
         update();
@@ -152,10 +168,13 @@ public class HangmanActivity extends AppCompatActivity {
             update();
             score++;
             disableAllButton(lst, true);
-
+            entered = new ArrayList();
+            save();
         } else if (answer.getHealth() == 0) {
             makeToastEntryText("Game Over");
             updateScoreBoard(game, name, score);
+            saveButton.setEnabled(false);
+            showScoreboard_1.setText("Show scoreboard");
         }
     }
 
@@ -186,25 +205,58 @@ public class HangmanActivity extends AppCompatActivity {
         return wordList;
     }
 
-    public void saveToFile(String fileName) {
+    private void loadGame() {
+        String FileName = name + "_" + game + "Infinity" + ".ser";
+        loadFromFile("answer" + FileName, "score" + FileName, "entered" + FileName);
+
+        update();
+        int index;
+        for (int i = 0; i < entered.size(); i++) {
+            index = alphabet.indexOf((char) entered.get(i));
+            entries[index].setEnabled(false);
+        }
+
+    }
+
+    private void save() {
+        String saveFileName = name + "_" + game + "Infinity" + ".ser";
+        saveToFile(answer, "answer" + saveFileName);
+        saveToFile(score, "score" + saveFileName);
+        saveToFile(entered, "entered" + saveFileName);
+        makeToastEntryText("Game Saved");
+    }
+
+    public void saveToFile(Object object, String fileName) {
         try {
             ObjectOutputStream outputStream = new ObjectOutputStream(
                     this.openFileOutput(fileName, MODE_PRIVATE));
-            outputStream.writeObject(answer);
+            outputStream.writeObject(object);
             outputStream.close();
         } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
     }
 
-    private void loadFromFile(String fileName) {
+    private void loadFromFile(String fileName1, String fileName2, String fileName3) {
 
         try {
-            InputStream inputStream = this.openFileInput(fileName);
+            InputStream inputStream = this.openFileInput(fileName1);
             if (inputStream != null) {
                 ObjectInputStream input = new ObjectInputStream(inputStream);
                 answer = (Word) input.readObject();
                 inputStream.close();
+            }
+            InputStream inputStream2 = this.openFileInput(fileName2);
+            if (inputStream2 != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream2);
+                score = (int) input.readObject();
+                inputStream2.close();
+            }
+            InputStream inputStream3 = this.openFileInput(fileName3);
+            if (inputStream3 != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream3);
+                entered = (ArrayList) input.readObject();
+                inputStream3.close();
             }
         } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
