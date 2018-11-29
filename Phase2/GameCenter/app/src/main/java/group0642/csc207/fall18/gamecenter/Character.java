7d13c450f5fedf54181d3f072d021e6305e2b6c3
baosight <dean.liu@mail.utoronto.ca>
2018-11-29 17:05:42 -0500

    Provided javadoc for all bottom java files except Word.java

--- Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/Character.java
+++ Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/Character.java
@@ -2,29 +2,55 @@ package group0642.csc207.fall18.gamecenter;
 
 import java.io.Serializable;
 
-public class Character  implements Serializable {
+class Character implements Serializable {
 
     private int health;
 
     private int attackDamage;
 
+    /**
+     * A new character.
+     *
+     * @param health the health of the character
+     * @param attackDamage the attack damage of the character
+     */
     Character(int health, int attackDamage) {
         this.setHealth(health);
         this.attackDamage = attackDamage;
     }
 
+    /**
+     * Return true iff the character is alive.
+     *
+     * @return isAlive as boolean
+     */
     boolean isAlive() {
         return getHealth() > 0;
     }
 
+    /**
+     * Return the attack damage
+     *
+     * @return attackDamage
+     */
     int getAttackDamage() {
         return attackDamage;
     }
 
+    /**
+     * Return the health
+     *
+     * @return health
+     */
     int getHealth() {
         return health;
     }
 
+    /**
+     * Change the health of the character
+     *
+     * @param health the new health of the character
+     */
     void setHealth(int health) {
         this.health = health;
     }
