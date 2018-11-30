f280bddb5cf81442bc2532b36908ee9b597c1fda
lucheng9 <ch.lu@mail.utoronto.ca>
2018-11-30 01:22:26 -0500

    Change public method to package-private.

--- Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/BankManager.java
+++ Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/BankManager.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 /**
  * manage money during the game
  */
-public class BankManager implements Serializable {
+class BankManager implements Serializable {
     private Integer wager;
     private Integer bank;
 
@@ -14,34 +14,37 @@ public class BankManager implements Serializable {
      * set the initial value of bank to 800
      * set the initial value of wager to 0
      */
-    public BankManager(){
+    BankManager() {
         bank = 800;
         wager = 0;
     }
 
     /**
      * return wager
+     *
      * @return wager
      */
-    Integer getWager(){
+    Integer getWager() {
         return wager;
     }
 
     /**
      * return bank
+     *
      * @return bank
      */
-    Integer getBank(){
+    Integer getBank() {
         return bank;
     }
 
     /**
      * add the input amount of money from bank to wager
+     *
      * @param w the amount of money you want to add
      * @return true if input <= bank, false otherwise
      */
-    boolean addWager(Integer w){
-        if(w <= bank){
+    boolean addWager(Integer w) {
+        if (w <= bank) {
             bank -= w;
             wager += w;
             return true;
@@ -52,11 +55,12 @@ public class BankManager implements Serializable {
     /**
      * add twice the wager to bank if game win, 0 otherwise
      * set wager to 0
+     *
      * @param result game result
      */
-    void checkOut(boolean result){
-        if(result) {
-            bank += 2* wager;
+    void checkOut(boolean result) {
+        if (result) {
+            bank += 2 * wager;
         }
         wager = 0;
     }
@@ -64,26 +68,32 @@ public class BankManager implements Serializable {
     /**
      * add all money in the bank to wager
      */
-    void allIn(){
+    void allIn() {
         addWager(bank);
     }
 
     /**
      * return whether the whole game is over
+     *
      * @return whether the whole game is over
      */
-    boolean gameOver(){ return getBank().equals(0) && getWager().equals(0);}
+    boolean gameOver() {
+        return getBank().equals(0) && getWager().equals(0);
+    }
 
     /**
      * return true if wager is zero
+     *
      * @return whether wager is zero or not
      */
-    boolean wagerIsZero(){return getWager().equals(0);}
+    boolean wagerIsZero() {
+        return getWager().equals(0);
+    }
 
     /**
      * put the wager back to the bank
      */
-    void undo(){
+    void undo() {
         bank = bank + wager;
         wager = 0;
     }
