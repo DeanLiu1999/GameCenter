b894ee352d8cc2494b411de5e2be8e98da09b8f2
baosight <dean.liu@mail.utoronto.ca>
2018-11-29 21:51:12 -0500

    Provided one additional unit test for SaveManager, still requires improvement from Cheng Lu

--- Phase2/GameCenter/app/src/test/java/group0642/csc207/fall18/gamecenter/SaveManagerTest.java
+++ Phase2/GameCenter/app/src/test/java/group0642/csc207/fall18/gamecenter/SaveManagerTest.java
@@ -2,7 +2,7 @@ package group0642.csc207.fall18.gamecenter;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class SaveManagerTest {
 
@@ -15,5 +15,26 @@ public class SaveManagerTest {
         String a2;
         a2 = (String) s.loadFromFile(filename);
         assertEquals("a", a2);
+    }
+
+    @Test
+    public void testUnexpectedCases(){
+        SaveManager s = new SaveManager();
+        boolean catch1 = false;
+        boolean catch2 = false;
+        try {
+            s.writeToFile("aaaaaa", "a");
+            assertEquals(0, 1);
+        } catch (NullPointerException e) {
+            catch1 = true;
+        }
+        String a2;
+        try {
+            a2 = (String) s.loadFromFile("bbbbbbbbbbbbbb");
+            assertEquals(0, 1);
+        } catch (NullPointerException e) {
+            catch2 = true;
+        }
+        assertTrue(catch1 && catch2);
     }
 }
