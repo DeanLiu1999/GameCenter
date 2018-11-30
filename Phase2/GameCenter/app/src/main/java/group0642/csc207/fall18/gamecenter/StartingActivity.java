ddb8a6ae8b7f6e1f9224a99feda8f98d0969f649
Yilun Li <bobliyilun.li@mail.utoronto.ca>
2018-11-29 22:21:44 -0500

    Merge branch 'master' of C:\Users\Yilun Li\Desktop\Phase_2 with conflicts.

--- Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/StartingActivity.java
+++ Phase2/GameCenter/app/src/main/java/group0642/csc207/fall18/gamecenter/StartingActivity.java
@@ -23,55 +23,44 @@ public class StartingActivity extends AppCompatActivity {
      * A temporary save file.
      */
     public static final String TEMP_SAVE_FILENAME = "save_file_temp.ser";
-    // private final String tag = "StartingActivity";
-
-    // TODO
-    private SaveManagerNew saveManager;
+    private static final String TAG = "StartingActivity";
 
     private String name;
     private String game;
 
+    private boolean loadIndicator = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getExternalFilesDir(null);//create the directory for file writing and reading
         setContentView(R.layout.activity_starting_);
-
         Intent inherit = getIntent();
         name = inherit.getStringExtra("name");
         game = inherit.getStringExtra("game");
-
-        saveManager = new SaveManagerNew.Builder()
-                .context(this)
-                .build();
-
         TextView title = findViewById(R.id.GameText);
         title.setText(String.format("Welcome to %s", game));
 
-        addStartButtonListener(name, game);
-        addLoadButtonListener(name, game);
+        addStartButtonListener();
+        addLoadButtonListener();
     }
 
     /**
      * Activate the start button.
-     *
-     * @param nm the username of the current active user
-     * @param gm the currently selected game
      */
-    private void addStartButtonListener(final String nm, final String gm) {
+    private void addStartButtonListener() {
         Button startButton = findViewById(R.id.StartButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (gm.equals("Blackjack")) {
-                    switchToBlackjack(nm, gm);
+                if (game.equals("Blackjack")) {
+                    switchToBlackjack();
 
-                } else if (gm.equals("Sliding Tiles")) {
+                } else if (game.equals("Sliding Tiles")) {
 
-                    switchToSlidingTilesSetting(nm, gm);
-                } else if (gm.equals("Hangman")) {
-                    switchToHangman(nm, gm);
+                    switchToSlidingTilesSetting();
+                } else if (game.equals("Hangman")) {
+                    switchToHangman();
                 }
             }
         });
@@ -80,21 +69,20 @@ public class StartingActivity extends AppCompatActivity {
 
     /**
      * Activate the load button.
-     *
-     * @param user the username of the current active user
-     * @param game the currently selected game
      */
-    private void addLoadButtonListener(final String user, final String game) {
+    private void addLoadButtonListener() {
         Button loadButton = findViewById(R.id.LoadButton);
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (game.equals("Sliding Tiles")) {
-                    LoadSlidingTiles(user, game);
+                    LoadSlidingTiles();
                 } else if (game.equals("Blackjack")) {
-                    LoadBlackjack(user, game);
+                    loadIndicator = true;
+                    LoadBlackjack();
                 } else if (game.equals("Hangman")) {
-                    LoadHangman(user, game);
+                    loadIndicator = true;
+                    switchToHangman();
                 }
 
             }
@@ -112,64 +100,53 @@ public class StartingActivity extends AppCompatActivity {
     }
 
     /**
-     * Switch to the SlidingTileSetting to configure the tile background.
-     *
-     * @param s the username of the current active user
-     * @param t the currently selected game
+     * Switch to the BackgroundSetting to configure the tile background.
      */
-    private void switchToSlidingTilesSetting(String s, String t) {
-        Intent GameSettingIntent = new Intent(this, SlidingTileSetting.class);
-        GameSettingIntent.putExtra("name", s);
-        GameSettingIntent.putExtra("game", t);
+    private void switchToSlidingTilesSetting() {
+        Intent GameSettingIntent = new Intent(this, BackgroundSetting.class);
+        GameSettingIntent.putExtra("name", name);
+        GameSettingIntent.putExtra("game", game
+        );
 
         StartingActivity.this.startActivity(GameSettingIntent);
     }
 
-    private void switchToBlackjack(String s, String t) {
-        Intent BlackjackGameIntent = new Intent(this, BlackjackGameActivity.class);
-        BlackjackGameIntent.putExtra("name", s);
-        BlackjackGameIntent.putExtra("game", t);
-        StartingActivity.this.startActivity(BlackjackGameIntent);
-
-    }
-
-    private void switchToHangman(String s, String t) {
-        Intent HangmanGameIntent = new Intent(this, HangmanModes.class);
-        HangmanGameIntent.putExtra("name", s);
-        HangmanGameIntent.putExtra("game", t);
-        StartingActivity.this.startActivity(HangmanGameIntent);
-    }
-
-    private void LoadSlidingTiles(String s, String t) {
+    private void LoadSlidingTiles() {
         Intent goToSaved = new Intent(StartingActivity.this, LoadActivity.class);
-        goToSaved.putExtra("name", s);
-        goToSaved.putExtra("game", t);
+        goToSaved.putExtra("name", name);
+        goToSaved.putExtra("game", game);
         StartingActivity.this.startActivity(goToSaved);
 
     }
 
-    private void LoadHangman(String s, String t) {
-        Intent HangmanLoadIntent = new Intent(this, HangmanModes.class);
-        HangmanLoadIntent.putExtra("name", s);
-        HangmanLoadIntent.putExtra("game", t);
-        HangmanLoadIntent.putExtra("load", true);
-        StartingActivity.this.startActivity(HangmanLoadIntent);
+    private void switchToBlackjack() {
+        Intent BlackjackGameIntent = new Intent(this, BlackjackGameActivity.class);
+        BlackjackGameIntent.putExtra("name", name);
+        BlackjackGameIntent.putExtra("game", game);
+        BlackjackGameActivity.load = loadIndicator;
+        StartingActivity.this.startActivity(BlackjackGameIntent);
+
     }
 
-    private void LoadBlackjack(String s, String t) {
+    private void switchToHangman() {
+        Intent HangmanGameIntent = new Intent(this, HangmanModes.class);
+        HangmanGameIntent.putExtra("name", name);
+        HangmanGameIntent.putExtra("game", game);
+        HangmanGameIntent.putExtra("load", loadIndicator);
+        StartingActivity.this.startActivity(HangmanGameIntent);
+    }
+
+
+    private void LoadBlackjack() {
         String FileName = name + "_" + game + ".ser";
-        if(saveManager.loadFromFile("bankManager" + FileName) != null) {
-            Intent BlackjackLoadIntent = new Intent(this, BlackjackGameActivity.class);
-            BlackjackLoadIntent.putExtra("name", s);
-            BlackjackLoadIntent.putExtra("game", t);
-            BlackjackGameActivity.load = true;
-            StartingActivity.this.startActivity(BlackjackLoadIntent);
-        }else{
-            makeToastText("No Existing Save!");
+        if (loadFromFile("stateManager" + FileName) != null &&
+                loadFromFile("bankManager" + FileName) != null) {
+            switchToBlackjack();
+        } else {
+            Toast.makeText(this, "No Existing Save", Toast.LENGTH_SHORT).show();
         }
     }
 
-    /*
     private Object loadFromFile(String fileName) {
 
         try {
@@ -182,18 +159,13 @@ public class StartingActivity extends AppCompatActivity {
                 return object;
             }
         } catch (FileNotFoundException e) {
-            Log.e(tag, "File not found: " + e.toString());
+            Log.e(TAG, "File not found: " + e.toString());
         } catch (IOException e) {
-            Log.e(tag, "Can not read file: " + e.toString());
+            Log.e(TAG, "Can not read file: " + e.toString());
         } catch (ClassNotFoundException e) {
-            Log.e(tag, "File contained unexpected data type: " + e.toString());
+            Log.e(TAG, "File contained unexpected data type: " + e.toString());
         }
         return null;
-    }
-    */
-
-    private void makeToastText(String text) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
 }
